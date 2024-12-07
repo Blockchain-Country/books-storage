e19d8aaf16014ccb0716056e045b9b21aeb9dac5
Kot Anton <707myemail@gmail.com>
2024-12-07 18:51:19 -0500

    added or condition for .env variables

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -3,12 +3,15 @@ import { getAuth } from 'firebase/auth'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'test_api_key',
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || 'test_auth_domain',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'test_project_id',
+  storageBucket:
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'test_storage_bucket',
+  messagingSenderId:
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ||
+    'test_messaging_sender_id',
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || 'test_app_id',
 }
 
 const app = initializeApp(firebaseConfig)
